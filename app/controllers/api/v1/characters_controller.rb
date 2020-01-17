36ef0e0f0956ab42eff2f4101497fc288964a48e
Luis F. Bilecki <luis.bilecki@gmail.com>
2020-01-16 21:37:51 -0300

    Remove  unnecessary comments in controller

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -4,16 +4,11 @@ module Api
       before_action :validate_id
 
       def show
-        # Initialize API Fetcher
         fetcher = RickyAndMorty::Fetcher.new
         character_id = params[:id]
 
-        # Cached data? 
         result = Rails.cache.fetch("character-#{character_id}", expires_in: 1.day) do  
-          # Get character data (first episode)
-          character_data = fetcher.fetch_single_character(character_id)
-  
-          # Get episode data 
+          character_data = fetcher.fetch_single_character(character_id)  
           episode_data = fetcher.fetch_episode(character_data[:first_episode])
   
           episode_data[:air_date]
@@ -28,7 +23,6 @@ module Api
   
       # If we need to validate body, is it better to use https://github.com/ruby-json-schema/json-schema
       def validate_id
-        # Accept only number as id
         return if Integer(params[:id]) rescue nil?
   
         render json: { status: 'error', message: 'invalid id' }, status: :unprocessable_entity
